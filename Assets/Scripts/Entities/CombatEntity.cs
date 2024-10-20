6225cfa2eb873c988f2be17367753beafaba075d
JosephPe <ps4daize@gmail.com>
2024-10-19 19:12:57 -0600

    URP and Sprites
    This project was updated to URP, and some sprites were added

--- Assets/Scripts/Entities/CombatEntity.cs
+++ Assets/Scripts/Entities/CombatEntity.cs
@@ -11,17 +11,27 @@ public class CombatEntity : MonoBehaviour
     [SerializeField] EntitySO entity;
     public IntGameEvent healthChange;
 	[SerializeField] Scrollbar Health;
-
+    [SerializeField] GameObject myTurn;
+    private bool isMyTurn = false;
 
 	// Start is called before the first frame update
 	void Awake()
     {
-
+        
 	}
 
     // Update is called once per frame
     void Update()
     {
-        
+        if(isMyTurn)
+        {
+            myTurn.SetActive(true);
+        }
+        else
+        {
+            myTurn.SetActive(false);
+        }
     }
+
+
 }
